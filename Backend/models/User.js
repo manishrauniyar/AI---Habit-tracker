2aee94396127f635b8b85661d3dec7ea0e38bc4f
manishrauniyar <manishrauniya8736r@gmail.com>
2026-08-12 20:33:45 +0530

    used bcrypt

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -2,45 +2,8 @@
 
 
 
-// import mongoose from "mongoose";
-// import bcrypt from "bcryptjs";
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     name: { type: String, required: true, trim: true },
-//     email: { type: String, required: true, unique: true, trim: true, lowercase: true },
-//     password: { type: String, required: true, minlength: 6 },
-//     avatar: { type: String, default: "" },
-//     morningMotivation: { type: Boolean, default: false },   
-//   },
-//   { timestamps: true }
-// );
-
-// userSchema.pre("save", async function () {
-//   if (!this.isModified("password")) return;
-
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-// });
-
-// userSchema.methods.matchPassword = function (plain) {
-//     return bcrypt.compare(plain, this.password);
-// };
-
-// userSchema.methods.toJSON = function () {
-//     const obj = this.toObject();
-//     delete obj.password;
-//     return obj;
-// };
-
-// const User = mongoose.model("User", userSchema);
-
-// export default User;
-
-
-
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -48,35 +11,77 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true, trim: true, lowercase: true },
     password: { type: String, required: true, minlength: 6 },
     avatar: { type: String, default: "" },
-    morningMotivation: { type: Boolean, default: false },
-
-    //added
+    morningMotivation: { type: Boolean, default: false },   
     role: {
       type: String,
       enum: ["user", "admin"],
       default: "user",
     },
-
-    
   },
   { timestamps: true }
 );
 
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 userSchema.methods.matchPassword = function (plain) {
-  return plain === this.password;
+    return bcrypt.compare(plain, this.password);
 };
-
 
 userSchema.methods.toJSON = function () {
-  const obj = this.toObject();
-  delete obj.password;
-  return obj;
+    const obj = this.toObject();
+    delete obj.password;
+    return obj;
 };
-
-
 
 const User = mongoose.model("User", userSchema);
 
-
 export default User;
+
+
+
+// import mongoose from "mongoose";
+
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true, trim: true },
+//     email: { type: String, required: true, unique: true, trim: true, lowercase: true },
+//     password: { type: String, required: true, minlength: 6 },
+//     avatar: { type: String, default: "" },
+//     morningMotivation: { type: Boolean, default: false },
+
+//     //added
+//     role: {
+//       type: String,
+//       enum: ["user", "admin"],
+//       default: "user",
+//     },
+
+    
+//   },
+//   { timestamps: true }
+// );
+
+
+// userSchema.methods.matchPassword = function (plain) {
+//   return plain === this.password;
+// };
+
+
+// userSchema.methods.toJSON = function () {
+//   const obj = this.toObject();
+//   delete obj.password;
+//   return obj;
+// };
+
+
+
+// const User = mongoose.model("User", userSchema);
+
+
+// export default User;
